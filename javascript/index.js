7425513259f1ebdae35e44e28426b1c634c8f886
John <johnmulvey48@yahoo.com>
2022-03-14 15:43:45 -0400

    22

--- javascript/index.js
+++ javascript/index.js
@@ -15,6 +15,16 @@ function updateDisplay(){
     likesDisplay.innerHTML = likes;
 };
 
+document.querySelector('#commentSection').addEventListener('submit', handleSubmit)
+
+function handleSubmit(e) {
+    e.preventDefault()
+    let commentObject = {
+        comment:e.target.comment.value
+    }
+}
+renderOneComment(commentObject)
+
 function renderOneComment(comment) {
     let card = document.createElement('li')
     card.className = 'card'
@@ -25,9 +35,12 @@ document.querySelector('#comment-section').appendChild(card)
 
 function getAllComments() {
     fetch('http://localhost3000/comments')
+    .then(res => res.json())
+    .then(comment => console.log(data))
 }
 
 function initialize() {
-
+    getAllComments()
+    console.log()
 }
 initialize()
