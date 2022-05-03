bc5cf35083283bdb2108f3fef5aaec1f54b532da
John <johnmulvey48@yahoo.com>
2022-05-03 13:30:42 -0400

    Functionality changes, added database tracking for comments and likes

--- javascript/index.js
+++ javascript/index.js
@@ -2,12 +2,28 @@ const likesDisplay = document.querySelector('.displayLikes');
 
 const likesPlus = document.querySelector('.likesAdd');
 
-let likes = 0;
+let likes;
+
+fetch('http://localhost:3000/likes')
+.then(res => res.json())
+.then(likesObj => {
+    likes = likesObj.likesCount    
+    updateDisplay()
+})
+.catch(err => console.log(err))
 
 likesPlus.addEventListener("click", () => {
     likes++;
     updateDisplay();
-}) ;
+    fetch('http://localhost:3000/likes', {
+        method: 'PATCH',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({likesCount: likes})
+    })
+});
+
 
 function updateDisplay() {
     likesDisplay.innerHTML = likes;
@@ -17,17 +33,23 @@ document.querySelector('#comment-section').addEventListener('submit', handleSubm
 
 function handleSubmit(e) {
     e.preventDefault()
-
     let commentText = {
         comment:e.target.commentBox.value
     }
-
     renderOneComment(commentText)
 }
 
-function renderOneComment({comment}) {
-    let card = document.createElement('li')
-    card.innerText = comment
+function renderOneComment({id, comment}) {
+    let card = document.createElement('div')
+    let cardLi = document.createElement('li')
+    cardLi.innerText = comment
+    const deleteButton = document.createElement('button')
+    deleteButton.addEventListener('click', () => {
+        handleDelete(id)
+        card.remove()
+    })
+    deleteButton.innerText = "X"
+    card.append(cardLi, deleteButton)
     document.querySelector('#comments').appendChild(card)
 }
 
@@ -35,11 +57,22 @@ function getAllComments() {
     fetch('http://localhost:3000/comments')
     .then(res => res.json())
     .then(commentText => {
-        console.log("a")
+        console.log(commentText)
         commentText.forEach(comment => renderOneComment(comment))
-    })
-    console.log("b")
+    }) 
+    .catch(err => console.log(err))
 }
+
+function handleDelete(commentId) {
+    fetch(`http://localhost:3000/comments/${commentId}`, {
+        method: 'DELETE',
+        header: {
+            'content-type': 'application/json'
+        }
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+}  
 
 function initialize() {
     getAllComments()
@@ -47,10 +80,15 @@ function initialize() {
 
 initialize()
 
-
 const button = document.getElementById("post"); 
 
 function alertButton() {
     alert('Bark Bark! Thank you!');
 }
     button.addEventListener("click", alertButton);
+
+const newButton = document.querySelector('#newButton')
+newButton.addEventListener('click', () => {
+  const li = document.createElement('li')
+  newButton.appendChild(li)
+})
